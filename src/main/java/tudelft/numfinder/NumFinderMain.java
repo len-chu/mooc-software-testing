421e440d7c6bc5d0a021a0c0e2e85953f1e40b58
enilina <enilina@yoomoney.ru>
2023-02-15 10:56:08 +0300

    fix numfinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -11,6 +11,7 @@ public class NumFinderMain {
         // this crashes
         nf.find(new int[] {4, 3, 2, 1});
 
+
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
     }
